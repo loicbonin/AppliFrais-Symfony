29a5cfcc5faae523f7d6f287e4fd06dc986d1546
loicbonin <loic.bonin@live.fr>
2018-04-12 21:25:35 +0200

    ise à jour javascript

--- web/js/main.js
+++ web/js/main.js
@@ -1,32 +1,71 @@
 $(document).ready(function(){
     $('.modal').modal();
+    $('.collapsible').collapsible();
+
+
+    $('.datepicker').datepicker({
+        nextMonth: 'Mois suivant',
+        previousMonth: 'Mois précédent',
+        labelMonthSelect: 'Selectionner le mois',
+        labelYearSelect: 'Selectionner une année',
+        months: [ 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre' ],
+        monthsShort: [ 'Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Jun', 'Jul', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec' ],
+        weekdays: [ 'Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi' ],
+        weekdaysShort: [ 'Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam' ],
+        weekdaysAbbrev: [ 'D', 'S', 'T', 'Q', 'Q', 'S', 'S' ],
+        today: 'Aujourd\'hui',
+        clear: 'Réinitialiser',
+        done: 'Valider',
+        cancel: 'fermer',
+        format: 'dd/mm/yyyy',
+        firstDay: 1
+    });
 });
 
+(function($) {
+    window.materializeForm.initDate = function() {
+        $('input.date').datepicker({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 2, // Creates a dropdown of 15 years to control year
+            labelMonthNext: 'Mois suivant',
+            labelMonthPrev: 'Mois précédent',
+            labelMonthSelect: 'Selectionner le mois',
+            labelYearSelect: 'Selectionner une année',
+            monthsFull: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+            monthsShort: ['Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Jun', 'Jul', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec'],
+            weekdaysFull: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+            weekdaysShort: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+            weekdaysLetter: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+            today: 'Aujourd\'hui',
+            clear: 'Réinitialiser',
+            close: 'Fermer',
+            format: 'dd/mm/yyyy'
+        });
+    }
+})(jQuery);
+
+$('.datepicker').datepicker({
+    nextMonth: 'Mois suivant',
+    previousMonth: 'Mois précédent',
+    labelMonthSelect: 'Selectionner le mois',
+    labelYearSelect: 'Selectionner une année',
+    months: [ 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre' ],
+    monthsShort: [ 'Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Jun', 'Jul', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec' ],
+    weekdays: [ 'Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi' ],
+    weekdaysShort: [ 'Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam' ],
+    weekdaysAbbrev: [ 'D', 'S', 'T', 'Q', 'Q', 'S', 'S' ],
+    today: 'Aujourd\'hui',
+    clear: 'Réinitialiser',
+    done: 'Valider',
+    cancel: 'fermer',
+    format: 'dd/mm/yyyy',
+    firstDay: 1
+});
+
+/*
 var instance = M.FeatureDiscovery.getInstance(elem);
 
 $(document).ready(function(){
-    $('.tap-target').featureDiscovery();
+$('.tap-target').featureDiscovery();
 });
-
-$('.timepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 5, // Creates a dropdown of 15 years to control year,
-    today: 'Aujourd\'hui',
-    clear: 'Vider',
-    close: 'Ok',
-    closeOnSelect: false, // Close upon selecting a date,// The title label to use for the month nav buttons
-    labelMonthNext: 'Mois suivant',
-    labelMonthPrev: 'Mois precedent',
-    // The title label to use for the dropdown selectors
-    labelMonthSelect: 'Selectionner un mois',
-    labelYearSelect: 'Selectionner une année',
-	// Months and weekdays
-    monthsFull: [ 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre' ],
-    monthsShort: [ 'Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Jui', 'Jul', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec' ],
-    weekdaysFull: [ 'Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi' ],
-    weekdaysShort: [ 'DIm', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam' ],
-    // Materialize modified
-    weekdaysLetter: [ 'D', 'L', 'M', 'M', 'J', 'V', 'S' ],
-    // The format to show on the `input` element
-    format: 'yyyy-mm-dd',
-});
+*/
