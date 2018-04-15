658f86022689f56a694084f5e31d84307852f8f1
loicbonin <loic.bonin@live.fr>
2018-04-15 19:50:54 +0200

    ajout redirection pour administrateur

--- src/UserBundle/Authentication/Authentication.php
+++ src/UserBundle/Authentication/Authentication.php
@@ -24,14 +24,20 @@ class Authentication implements AuthenticationSuccessHandlerInterface
     {
         if ($this->security->isGranted('ROLE_ADMIN'))
         {
+            // redirect the user to where they were before the login process begun.
+            $response = new RedirectResponse($this->router->generate('index_super_admin'));
+        }
+        elseif ($this->security->isGranted('ROLE_COMPTABLE'))
+        {
             $response = new RedirectResponse($this->router->generate('index_admin'));
 
         }
-        elseif ($this->security->isGranted('ROLE_USER'))
+        elseif ($this->security->isGranted('ROLE_USER') OR $this->security->isGranted('ROLE_VISITEUR'))
         {
             // redirect the user to where they were before the login process begun.
             $response = new RedirectResponse($this->router->generate('fichefrais_index'));
         }
+
 
         return $response;
     }
