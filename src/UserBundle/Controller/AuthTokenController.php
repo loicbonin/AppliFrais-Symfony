ddecc340c80c6907ed17a37957e377d75dcd5bee
loicbonin <loic.bonin@live.fr>
2018-04-21 16:10:39 +0200

    connexion à la base et vérification avec application android

--- src/UserBundle/Controller/AuthTokenController.php
+++ src/UserBundle/Controller/AuthTokenController.php
@@ -13,37 +13,47 @@ use UserBundle\Form\CredentialsType;
 use UserBundle\Entity\AuthToken;
 use UserBundle\Entity\Credentials;
 
+
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\View\ViewHandler;
+use FOS\RestBundle\View\View;
+
 class AuthTokenController extends Controller
 {
-    /*
+    /**
      * @Rest\View(statusCode=Response::HTTP_CREATED, serializerGroups={"auth-token"})
-     * @Rest\Post("/json/auth-tokens")
-     *
+     * @Rest\Get("/json/auth-tokens")
+     */
     public function postAuthTokensAction(Request $request)
     {
         $credentials = new Credentials();
-        $form = $this->createForm(CredentialsType::class, $credentials);
+        $credentials->setLogin($_GET['login']);
+        $credentials->setPassword($_GET['password']);
+
+        /*$form = $this->createForm(CredentialsType::class, $credentials);
 
         $form->submit($request->request->all());
 
         if (!$form->isValid()) {
             return $form;
-        }
+        }*/
 
         $em = $this->get('doctrine.orm.entity_manager');
 
         $user = $em->getRepository('UserBundle:User')
-            ->findOneByEmail($credentials->getLogin());
+            ->findOneByUsername($credentials->getLogin());
 
         if (!$user) { // L'utilisateur n'existe pas
-            return $this->invalidCredentials();
+            //return $this->invalidCredentials();
+            return  ['AUTH_STATUS' => 'False'];
         }
 
         $encoder = $this->get('security.password_encoder');
         $isPasswordValid = $encoder->isPasswordValid($user, $credentials->getPassword());
 
         if (!$isPasswordValid) { // Le mot de passe n'est pas correct
-            return $this->invalidCredentials();
+            //return $this->invalidCredentials();
+            return  ['AUTH_STATUS' => 'False'];
         }
 
         $authToken = new AuthToken();
@@ -54,7 +64,22 @@ class AuthTokenController extends Controller
         $em->persist($authToken);
         $em->flush();
 
-        return $authToken;
+
+        $formatted = [
+            'AUTH_STATUS' => 'True',
+            'id' => $user->getId(),
+            'token' => $authToken->getValue(),
+            'identity' => $user->getFirstName()." ".$user->getLastName(),
+
+
+        ];
+
+        $view = View::create($formatted);
+        $view->setFormat('json');
+
+        return $view;
+
+        //return $authToken->getId();
     }
 
     private function invalidCredentials()
@@ -62,16 +87,16 @@ class AuthTokenController extends Controller
         return \FOS\RestBundle\View\View::create(['message' => 'Invalid credentials'], Response::HTTP_BAD_REQUEST);
     }
 
-    /*
+    /**
      * @Rest\View(statusCode=Response::HTTP_NO_CONTENT)
      * @Rest\Delete("/json/auth-tokens/{id}")
-     *
+     */
     public function removeAuthTokenAction(Request $request)
     {
         $em = $this->get('doctrine.orm.entity_manager');
         $authToken = $em->getRepository('UserBundle:AuthToken')
             ->find($request->get('id'));
-        /* @var $authToken AuthToken *
+        /* @var $authToken AuthToken */
 
         $connectedUser = $this->get('security.token_storage')->getToken()->getUser();
 
@@ -81,5 +106,5 @@ class AuthTokenController extends Controller
         } else {
             throw new \Symfony\Component\HttpKernel\Exception\BadRequestHttpException();
         }
-    }*/
+    }
 }
--- src/UserBundle/Repository/UserRepository.php
+++ src/UserBundle/Repository/UserRepository.php
@@ -23,4 +23,18 @@ class UserRepository extends \Doctrine\ORM\EntityRepository
 
 
     }*/
+
+    /*public function getUserByRole()
+    {
+        $request = $this->createQueryBuilder('u')
+            ->where('u.roles = a:0:{}' )
+            ->andWhere('u.roles = a:1:{i:0;s:9:"ROLE_USER";}')
+            ->orderBy('u.lastName', 'DESC')
+            ->getQuery()
+            ->execute()
+        ;
+        return $request;
+
+
+    }*/
 }
